e40b6d2a1afbae94d6d6a2077ab8f47f26b8eb34
Ezrqn Kemboi Dev <ezkemboi@Emmanuels-MacBook-Pro.local>
2022-11-17 17:57:54 +0300

    feat(plans): plans endpoints

---
+++ src/endpoints/plans.js
@@ -0,0 +1,14 @@
+const route = '/plan'
+
+const plans = {
+  createPlan: {
+    method: "post",
+    route: route,
+    bodyParams: {
+      required: [],
+      optional: []
+    }
+  }
+}
+
+export default plans
