45dc84311aa383b064c97baa77dd004cc9d10f7f
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2022-11-17 19:43:23 +0300

    feature(plans): plans endpoints

--- src/endpoints/plans.js
+++ src/endpoints/plans.js
@@ -5,10 +5,20 @@ const plans = {
     method: "post",
     route: route,
     bodyParams: {
-      required: [],
-      optional: []
+      required: ["name", "amount", "interval"],
+      optional: ["description", "send_invoices", "send_sms", "currency", "invoice_limit"]
     }
+  },
+  listPlans: {
+    method: "get",
+    route: route,
+    queryParams: {
+      required: ["perPage", "page"],
+      optional: ["status", "interval", "amount",]
+    }
+
   }
+
 }
 
 export default plans
