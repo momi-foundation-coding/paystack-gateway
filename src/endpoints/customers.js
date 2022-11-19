ef2023cd1f308c407c6b7dc2d5ad406d36257538
ezkemboi <ezrqnkemboi@gmail.com>
2022-11-19 19:24:57 +0300

    chore(formatting): add correct formatting

--- src/endpoints/customers.js
+++ src/endpoints/customers.js
@@ -1,74 +1,74 @@
 const route = "/customer";
 
 const customers = {
-    createCustomer:{
-        method:"post",
-        route:route,
-        bodyParams:{
-            required:["email", "first_name","last_name"],
-            optional:["phone", "metadata"]
-        }
-    },
-    listCustomers:{
-        method:"get",
-        route:route,
-        queryParams:{
-            required:["perPage", "page"],
-            optional:["from", "to"]
-        }
-    },
-    fetchCustomer:{
-        method:"get",
-        route:route + "/:email_or_code",
-        pathParams:{
-            required:["email_or_code"],
-            optional:[]
-        }
-    },
-    updateCustomer:{
-        method:"put",
-        route:route + "/:code",
-        queryParams:{
-            required:["code"],
-            optional:[]
-        },
-        bodyParams:{
-            required:["first_name", "last_name"],
-            optional:["phone", "metadata"]
-        }
-    },
-    validateCustomer:{
-        method:"post",
-        route: route + "/:code/identification",
-        bodyParams:{
-            required:["first_name", "last_name", "type", "value", "country", "bvn",  "bank_code", "account_number"],
-            optional:["middle_name"]
-        }
-    },
-    whitelistCustomer:{
-        method:"post",
-        route:route + "/set_risk_action",
-        bodyParams:{
-            required:["customer"],
-            optional:["risk_action"]
-        }
-    },
-    blacklistCustomer:{
-        method:"post",
-        route:route + "/set_risk_action",
-        bodyParams:{
-            required:["customer"],
-            optional:["risk_action"]
-        }
-    },
-    deactivateAuthorization:{
-        method:"post",
-        route:route + "/deactivate_authorization",
-        bodyParams:{
-            required:["authorization_code"],
-            optional:[]
-        }
+  createCustomer:{
+    method:"post",
+    route:route,
+    bodyParams:{
+      required:["email", "first_name","last_name"],
+      optional:["phone", "metadata"]
     }
+  },
+  listCustomers:{
+    method:"get",
+    route:route,
+    queryParams:{
+      required:["perPage", "page"],
+      optional:["from", "to"]
+    }
+  },
+  fetchCustomer:{
+    method:"get",
+    route:route + "/:email_or_code",
+    pathParams:{
+      required:["email_or_code"],
+      optional:[]
+    }
+  },
+  updateCustomer:{
+    method:"put",
+    route:route + "/:code",
+    queryParams:{
+      required:["code"],
+      optional:[]
+    },
+    bodyParams:{
+      required:["first_name", "last_name"],
+      optional:["phone", "metadata"]
+    }
+  },
+  validateCustomer:{
+    method:"post",
+    route: route + "/:code/identification",
+    bodyParams:{
+      required:["first_name", "last_name", "type", "value", "country", "bvn",  "bank_code", "account_number"],
+      optional:["middle_name"]
+    }
+  },
+  whitelistCustomer:{
+    method:"post",
+    route:route + "/set_risk_action",
+    bodyParams:{
+      required:["customer"],
+      optional:["risk_action"]
+    }
+  },
+  blacklistCustomer:{
+    method:"post",
+    route:route + "/set_risk_action",
+    bodyParams:{
+      required:["customer"],
+      optional:["risk_action"]
+    }
+  },
+  deactivateAuthorization:{
+    method:"post",
+    route:route + "/deactivate_authorization",
+    bodyParams:{
+      required:["authorization_code"],
+      optional:[]
+    }
+  }
 }
 
 export default customers 
