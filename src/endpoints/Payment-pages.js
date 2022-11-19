4e58954270372950001f0bcd6dd4dac5ceb2aab0
Stanley <78428936+chepkok3@users.noreply.github.com>
2022-11-19 11:52:52 +0300

    created the payment pages endpoint with create, read, update and delete functions

---
+++ src/endpoints/Payment-pages.js
@@ -0,0 +1,64 @@
+const route = "/payment";
+
+const paymentPages = {
+  
+  createPages:{
+    route:route,
+    method:"post",
+    bodyParams:{
+        required:["name"],
+        optional:["description", "amount", "slug", "metadata", "redirect_url", "custom_fields"]
+    },
+  },
+    listPages:{
+      method: "get",
+      route: route,
+      queryParams: {
+        required: ["perPage", "page"],
+        optional: ["from", "to",]
+      }
+    },
+
+      fetchPage:{
+        method: "get",
+        route: route +":/id",
+        queryParams: {
+          required: ["id"],
+          optional: []
+        }
+      },
+      updatePage:{
+        method:"put",
+        route: route +":/id",
+        queryParams: {
+          required: ["id"],
+          optional: []
+        },
+        bodyParams {
+          required: ["name", "description"],
+          optional: ["amount", "active"]
+        }
+      },
+      checkSlugAvailability:{
+        method:"post",
+        route: route,
+        pathParams:{
+          required: ["slig"],
+          optional: []
+        },
+      }
+      addProducts:{
+        method: "put",
+        route: route,
+        Headers:{
+          required: ["id"],
+          optional: []
+        },
+        bodyParams:{
+          required: ["product"],
+          optional: []
+        }
+      }
+}
+
+export default paymentPages
