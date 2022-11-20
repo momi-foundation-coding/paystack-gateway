38f3e698f2441e85422cccbd6904320b3392a26b
ezkemboi <ezrqnkemboi@gmail.com>
2022-11-20 06:51:14 +0300

    fix(bugs): small bugs fixes

--- src/resources.js
+++ src/resources.js
@@ -1,7 +1,7 @@
 // import different resource with its methods
 import transactions from "./endpoints/transactions.js";
-import customers from "./endpoints/customers";
-import transactionSplits from "./endpoints/transaction_splits";
+import customers from "./endpoints/customers.js";
+import transactionSplits from "./endpoints/transaction_splits.js";
 import products from "./endpoints/products.js";
 import subscriptions from "./endpoints/subscriptions.js";
 import plans from "./endpoints/plans.js";
